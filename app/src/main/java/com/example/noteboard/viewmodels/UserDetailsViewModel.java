c2e5af822d423e467530e873eca00da7cc993601
Opilane TTHK <opilane@tthk.ee>
2022-05-19 14:03:12 +0300

    update

--- app/src/main/java/com/example/noteboard/viewmodels/UserDetailsViewModel.java
+++ app/src/main/java/com/example/noteboard/viewmodels/UserDetailsViewModel.java
@@ -1,6 +1,7 @@
 package com.example.noteboard.viewmodels;
 
 import android.app.Application;
+import android.widget.EditText;
 
 import androidx.annotation.NonNull;
 import androidx.lifecycle.AndroidViewModel;
@@ -14,6 +15,10 @@ public class UserDetailsViewModel extends AndroidViewModel {
     public UserDetailsViewModel(@NonNull Application application) {
         super(application);
         authRepository = new AuthRepository(application);
+    }
+
+    public void updateData(EditText editTextEmail, EditText editTextUserName, EditText editTextPassword){
+        authRepository.updateDataFromForm(editTextEmail,editTextUserName,editTextPassword);
     }
 
     public void resetPassword(){
