2b6e6372deab1de095eb3614528c81a116171baa
KoppelTAR <punanepikne@gmail.com>
2022-05-27 23:45:48 +0300

    Add landscape for sms frag and replace back buttons with back arrows

--- app/src/main/java/com/example/noteboard/fragments/SmsFragment.java
+++ app/src/main/java/com/example/noteboard/fragments/SmsFragment.java
@@ -2,11 +2,14 @@ package com.example.noteboard.fragments;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 import androidx.navigation.Navigation;
 
 import android.view.LayoutInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.EditText;
@@ -15,6 +18,8 @@ import android.widget.Toast;
 import com.example.noteboard.R;
 import com.example.noteboard.Utils;
 import com.example.noteboard.viewmodels.SMSViewModel;
+
+import java.util.Objects;
 
 
 public class SmsFragment extends Fragment {
@@ -32,18 +37,9 @@ public class SmsFragment extends Fragment {
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_sms, container, false);
         EditText numberEditText = view.findViewById(R.id.editTextPhoneNumber);
-        view.findViewById(R.id.buttonSMSBack).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Bundle bundle = new Bundle();
-                bundle.putString("title",getArguments().getString("title"));
-                bundle.putString("content",getArguments().getString("content"));
-                bundle.putLong("sharingcode",getArguments().getLong("sharingcode"));
-                bundle.putString("author", getArguments().getString("author"));
-                bundle.putString("type",getArguments().getString("type"));
-                Navigation.findNavController(getView()).navigate(R.id.action_smsFragment_to_singlePostFragment,bundle);
-            }
-        });
+        Objects.requireNonNull(((AppCompatActivity) requireActivity()).getSupportActionBar()).setTitle(R.string.smsTitle);
+        setHasOptionsMenu(true);
+        Objects.requireNonNull(((AppCompatActivity) requireActivity()).getSupportActionBar()).setDisplayHomeAsUpEnabled(true);
         view.findViewById(R.id.btnSendPost).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -69,5 +65,19 @@ public class SmsFragment extends Fragment {
             }
         });
         return view;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if (item.getItemId() == android.R.id.home){
+            Bundle bundle = new Bundle();
+            bundle.putString("title",getArguments().getString("title"));
+            bundle.putString("content",getArguments().getString("content"));
+            bundle.putLong("sharingcode",getArguments().getLong("sharingcode"));
+            bundle.putString("author", getArguments().getString("author"));
+            bundle.putString("type",getArguments().getString("type"));
+            Navigation.findNavController(getView()).navigate(R.id.action_smsFragment_to_singlePostFragment,bundle);
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
